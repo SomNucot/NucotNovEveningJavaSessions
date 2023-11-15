34570a79b32f834210f76783293eb9e350d63428
Nucot-Som <136143072+SomNucot@users.noreply.github.com>
2023-11-15 21:48:22 +0530

    NovEveningFirstcommit

---
+++ src/JavaSessions/com/DemoOnDataTypesConcept.java
@@ -0,0 +1,157 @@
+package JavaSessions.com;
+
+public class DemoOnDataTypesConcept {
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		//Datatypes:
+		//1.Primitive data types:
+		
+		//boolean type: true/false
+		//Numeric Type:
+			//2.a.character type:-->char
+			//2.b.Integral type:
+				//2.b.a-->Integer: byte, short,int,long
+				//2.b.b--?Floating point:-->float , double
+		
+		//2.Non-Primitive data types:
+		//Object,classess,arrays,Interface,Abstract,Strings
+		
+	//1.Integer
+	//1.byte
+	//size:1byte---> 8 bits
+	//range:-128 to +127
+		
+		//synatx of variable
+		//data_type variable_name;
+		//Variable declaration and variable initializing
+		//example
+			byte  b;
+		
+		//variable initializing
+			byte  bb=10;
+			System.out.println(bb);
+			      bb=20;
+			System.out.println(bb);
+			
+			byte bbb=-128;
+			System.out.println(bbb);
+			
+			byte bbbb=127;
+			System.out.println(bbbb);
+			
+			
+	
+			System.out.println("*******Short data type*******");
+			//2.short
+			//size:2byte---> 2*8=16bits
+			//range:-32768 to +32767
+				
+				short s;
+				s=2;
+				System.out.println(s);
+				s=3;
+				System.out.println(s);
+				
+				short ss=-32768;
+				System.out.println(ss);
+			
+			
+				short sss=32767;
+				System.out.println(sss);
+			
+
+				short ssss=3276;
+				System.out.println(ssss);
+			
+			
+				
+				System.out.println("*******Int data type*******");
+				//3.int
+				//size:4byte---> 4*8=32bits
+				//range:-2147483648 to 2147483647
+					
+					int i;
+					i=2;
+					System.out.println(i);
+					i=3;
+					System.out.println(i);
+					
+					int ii=-2147483648;
+					System.out.println(ii);
+				
+				
+					int iii=2147483647;
+					System.out.println(iii);
+				
+
+					int iiii=327644;
+					System.out.println(iiii);
+				
+					
+					System.out.println("*******Long data type*******");
+					//4.long
+					//size:8byte---> 8*8=64bits
+					//range:-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+					//-2^31-1 to 2^31-1	
+						long l ;
+						l=2;
+						System.out.println(l);
+						l=3;
+						System.out.println(l);
+						
+						long ll=-2147483648;
+						System.out.println(ll);
+					
+					
+						long lll=2147483647;
+						System.out.println(lll);
+					
+
+						long llll=327644l;
+						System.out.println(llll);
+					
+						long contactNumber=12345678910l;
+						System.out.println(contactNumber);
+					
+			
+						System.out.println("*******Float data type*******");
+						//5.float
+						//size:4byte---> 4*8=32bits
+						//range:1.175494351 E - 38	3.402823466 E + 38
+							float f ;
+							f=2.0f;
+							System.out.println(f);
+							f=3.0f;
+							System.out.println(f);
+							float ff=(float)3.0;
+							System.out.println(ff);
+							
+							System.out.println("*******Float data type*******");
+							//6.double
+							//size:8byte---> 8*8=64bits
+							
+								double d ;
+								d=2.0000000;
+								System.out.println(f);
+								d=3.000000;
+								System.out.println(d);
+								double dd=13.00;
+								System.out.println(dd);			
+							
+							
+							
+							
+
+
+				
+		
+		
+		
+
+	}
+
+}
