7d7006093dce6c6f0016e0886fa1b7df7d4a53c5
Nucot-Som <136143072+SomNucot@users.noreply.github.com>
2023-11-20 21:42:55 +0530

    IncrementandDecrement|ConditionalStaementDemo

---
+++ src/JavaSessions/com/ConditionalStaementDemo.java
@@ -0,0 +1,109 @@
+package JavaSessions.com;
+
+public class ConditionalStaementDemo {
+
+	public static void main(String[] args) {
+		
+		//if statement
+		//if else statement
+		//nested if else statement
+		//switch statement
+		
+	
+		int i=10;
+		int j=20;
+		System.out.println(i==j);
+		
+		//if statement
+		/*
+		 * syntax
+		 * 
+		 * if(condition)
+		 * {
+		 * 		body//statements
+		 * 	
+		 * }
+		 * System.out.println("Thanks");
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		int ii=5;
+		int jj=5;
+		if(ii==jj)
+		{
+			
+			System.out.println("Both are equal");
+			
+			
+		}
+		System.out.println("Bye");
+		
+		
+		
+		if(true)
+		{
+			System.out.println("Welcome  to nucot");
+		}
+		
+		//two way selection
+		//if else statement
+		
+		/*
+		 * 
+		 * if(condition)
+		 * {
+		 * 			body//statements
+		 * 
+		 * }
+		 * else
+		 * {
+		 * 			body//statements
+		 * 
+		 * }
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+	
+		int p=12;
+		int q=5;
+		
+		if(p>q)
+		{
+			System.out.println("P is greater");
+		}
+		else
+		{
+			System.out.println("Q is greater");
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/JavaSessions/com/DemoIncrementAndDecrement.java
@@ -0,0 +1,100 @@
+package JavaSessions.com;
+
+public class DemoIncrementAndDecrement {
+
+	public static void main(String[] args) {
+		
+		//++ and --
+		
+		//++-->Increment
+		//-->Decrement
+		
+		//Post Increment
+		//Post Decrement
+		
+		//Pre Increment
+		//Pre Decrement
+		
+		//Post Increment
+		
+		int i=2;
+		int j=i++;
+		System.out.println(i);//3
+		System.out.println(j);//2
+		
+		int ii=4;
+		int jj=ii++;
+		System.out.println(ii);//5
+		System.out.println(jj);//4
+		
+		
+		int iii=3;
+		int jjj=iii++;
+		System.out.println(iii);//4
+		System.out.println(jjj);//3
+		
+		
+		int p=-3;
+		int q=p++;
+		System.out.println(p);//-2
+		System.out.println(q);//-3
+		
+		
+		
+		
+		//Post Decrement
+		
+
+		int iiii=2;
+		int jjjj=iiii--;
+		System.out.println(iiii);//1
+		System.out.println(jjjj);//2
+		
+		
+		int iiiii=4;
+		int jjjjj=iiiii--;
+		System.out.println(iiiii);//3
+		System.out.println(jjjjj);//4
+		
+		
+		int pp=-4;
+		int qq=pp--;
+		System.out.println(pp);//-5
+		System.out.println(qq);//-4
+		
+		
+		
+		//Pre Increment
+		
+		int aa=2;
+		int bb=++aa;
+		System.out.println(aa);//3
+		System.out.println(bb);//3
+		
+		
+		int aaa=-4;
+		int bbb=++aaa;
+		System.out.println(aaa);//-3
+		System.out.println(bbb);//-3
+		
+		//Pre Decrement
+		
+		int e=2;
+		int f=--e;
+		System.out.println(e);//1
+		System.out.println(f);//1
+		
+		
+		int ee=-10;
+		int ff=--ee;
+		System.out.println(ee);//-11
+		System.out.println(ff);//-11
+		
+		
+		
+		
+		
+
+	}
+
+}
