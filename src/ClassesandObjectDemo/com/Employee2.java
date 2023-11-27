2f4a60c9a247b08d9361c4b406ce62eb667e20cc
Nucot-Som <136143072+SomNucot@users.noreply.github.com>
2023-11-27 21:36:06 +0530

    Classes and Object

---
+++ src/ClassesandObjectDemo/com/Employee2.java
@@ -0,0 +1,40 @@
+package ClassesandObjectDemo.com;
+
+public class Employee2 {
+
+	
+	//class variables or global variables
+	String name="Nucot";
+	int usn=123;
+	float salary=20.00f;
+	String address="Bangalore";
+	
+	//methods
+	
+	
+	//constructor
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+	
+		Employee2  obj=new Employee2();
+		
+		System.out.println(obj.name);
+		System.out.println(obj.usn);
+		System.out.println(obj.salary);
+		System.out.println(obj.address);
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/ClassesandObjectDemo/com/TypesOfMethodsDemo.java
@@ -0,0 +1,162 @@
+package ClassesandObjectDemo.com;
+
+public class TypesOfMethodsDemo {
+
+	
+	//normal methods----->Implemented method(Method def)(here Body  will be there) and
+	//non implemented method(Method prototype)(no body)
+	//static methods
+	
+	
+		//non implemented methods(method declaration)
+	
+		//syntax
+		// data_type method_name();-method protype with out arguments
+	    // data_type method_name(arguments);method protype with arguments
+	
+
+				//example: void add();
+				//	void add(int a,int b);
+				//	int add(int a,int b,int c);
+		        //	int add(int a,int b,int c,int d);
+		
+	//1.data_type method_name();no return type and no arguments
+	
+	//2.data_type method_name();having return type and no arguments
+	
+	//3.data_type method_name(arguments);-->No return type and with arguments
+	
+	//4.data_type method_name(arguments);-->having return type and with arguments
+	
+	
+	
+	//1.Implemented method--type one method
+	
+	//1.no return type and no arguments
+	
+	
+	/*note :no return type -->void
+	 * data_type method_name()
+	 * {
+	 * 
+	 * 			body
+	 * 
+	 * }
+	 * 
+	 * 
+	 * void abc()
+	 * {
+	 * 		body
+	 * 
+	 * }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	
+	
+	
+	//2.having return type and no arguments
+	
+	
+		/*note :no return type -->void
+		 * data_type method_name()
+		 * {
+		 * 
+		 * 			body
+		 * 			return statement;
+		 * }
+		 * 
+		 * 
+		 * int abc()
+		 * {
+		 * 		body
+		 * 
+		 * 	return statement;
+		 * }
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+	//3.No return type and with arguments
+	
+	
+			/*note :no return type -->void
+			 * data_type method_name(arguments)
+			 * {
+			 * 
+			 * 			body
+			 * 				return statement;
+			 * }
+			 * 
+			 * 
+			 * void abc(int a,int b)
+			 * {
+			 * 		body
+			 * 
+			 * 	
+			 * }
+			 * 
+			 * 
+			 * 
+			 * 
+			 * 
+			 * 
+			 */
+			
+		
+		
+	//4.having return type and with arguments
+	
+	
+	/*note :no return type -->void
+	 * data_type method_name(arguments)
+	 * {
+	 * 
+	 * 			body
+	 * 			return statement;	
+	 * }
+	 * 
+	 * 
+	 * int abc(int a,int b)
+	 * {
+	 * 		body
+	 * 
+	 * 		return statement;
+	 * }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
